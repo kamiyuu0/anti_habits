8b7ea67b9b56108e567961a130af382f5dc8df4a
yuekami <yuekami7@gmail.com>
2025-07-15 22:49:45 +0900

    feat: 悪習慣コントローラ作成

---
+++ app/helpers/anti_habits_helper.rb
@@ -0,0 +1,2 @@
+module AntiHabitsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions"
   }
 
   root "static_pages#top"
+
+  resources :anti_habits
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
