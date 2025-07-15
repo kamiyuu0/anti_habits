ee7839f1a98faa4c0a3a29b4d1cc7a3439950a22
yuekami <yuekami7@gmail.com>
2025-07-16 00:04:00 +0900

    feat: ログイン・サインアップ後のリダイレクト先を修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -27,11 +27,11 @@ class Users::SessionsController < Devise::SessionsController
 
   def after_sign_in_path_for(resource)
     # サインイン後にリダイレクトさせたいパスを記載
-    root_path
+    anti_habits_path
   end
 
   def after_sign_out_path_for(resource)
     # TODO: サインアウト後にリダイレクトさせたいパスを記載
-    new_user_session_path
+    root_path
   end
 end
