14e328b1418e473e87a1787d1ebcea5a00c65bac
yuekami <yuekami7@gmail.com>
2025-08-13 23:27:12 +0900

    feat: 応援コメントのエラーメッセージ対応

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,22 +3,25 @@ class CommentsController < ApplicationController
   before_action :set_anti_habit
 
   def create
-    comment = @anti_habit.comments.build(comment_params)
-    comment.user = current_user
-    if comment.save
+    @comment = @anti_habit.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
       redirect_to anti_habit_path(@anti_habit), notice: "応援メッセージを投稿しました"
     else
-      redirect_to anti_habit_path(@anti_habit), notice: "応援メッセージに失敗しました"
+      # エラー時はshowページを再表示（エラーメッセージ付き）
+      @today_record = @anti_habit.today_record if current_user&.own?(@anti_habit)
+      @comments = @anti_habit.comments.includes(:user).order(created_at: :desc)
+      render "anti_habits/show", status: :unprocessable_entity
     end
   end
 
   private
 
   def set_anti_habit
-    @anti_habit = AntiHabit.find(params[:anti_habit_id])
+    @anti_habit = AntiHabit.includes(:tags).find(params[:anti_habit_id])
   end
 
   def comment_params
-    params.require(:comment).permit(:body).merge(anti_habit_id: params[:anti_habit_id])
+    params.require(:comment).permit(:body)
   end
 end
