debde342dcd54d2b093e33f8130b5a7593e737b2
yuekami <yuekami7@gmail.com>
2025-08-13 22:02:54 +0900

    feat: RecordNotFound時のリダイレクト処理を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found
+    if user_signed_in?
+      redirect_to anti_habits_path, alert: "指定されたページが見つかりません。"
+    else
+      redirect_to root_path, alert: "指定されたページが見つかりません。"
+    end
+  end
 end
