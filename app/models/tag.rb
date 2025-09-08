d4290df80843c012a168f3aad8086c5f7d59cbca
yuekami <yuekami7@gmail.com>
2025-09-08 10:48:02 +0900

    feat: タグの最長を15文字に

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ApplicationRecord
   has_many :anti_habit_tags, dependent: :destroy
   has_many :anti_habits, through: :anti_habit_tags
 
-  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 15 }
 
   # namesのtagオブジェクトをdbから探す、なければ作成して返すメソッド。
   # 返り値はtagの配列
