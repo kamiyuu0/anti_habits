41a795f716281f9989ca5420c1cf0e5853ff247c
yuekami <yuekami7@gmail.com>
2025-11-06 13:21:49 +0900

    feat: リアクション機能に複数種類を想定し、ルーティングとコントローラを修正

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -2,11 +2,17 @@ class ReactionsController < ApplicationController
   before_action :authenticate_user!
   def create
     @anti_habit = AntiHabit.find(params[:anti_habit_id])
-    current_user.reaction(@anti_habit)
+    current_user.reaction(@anti_habit, reaction_params[:reaction_kind])
   end
 
   def destroy
-    @anti_habit = current_user.reactions.find(params[:id]).anti_habit
-    current_user.unreaction(@anti_habit)
+    @anti_habit = AntiHabit.find(params[:anti_habit_id])
+    current_user.unreaction(@anti_habit, reaction_params[:reaction_kind])
+  end
+
+  private
+
+  def reaction_params
+    params.require(:reaction).permit(:reaction_kind)
   end
 end
