4a75018d0b55692ef023cd5673c20ca8c022984b
yuekami <yuekami7@gmail.com>
2025-11-06 13:20:49 +0900

    feat: リアクション機能に複数種類を想定し関連メソッドを修正

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -2,6 +2,6 @@ class Reaction < ApplicationRecord
   belongs_to :anti_habit
   belongs_to :user
 
-  validates :user_id, uniqueness: { scope: :anti_habit_id }
+  validates :user_id, uniqueness: { scope: [:anti_habit_id, :reaction_kind] }
   enum reaction_kind: { watching: 0 }
 end
