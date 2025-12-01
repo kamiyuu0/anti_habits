442e936b47710d91d180d44f77e76e8956b31c0c
yuekami <yuekami7@gmail.com>
2025-12-01 22:55:34 +0900

    feat: タグ機能にtagifyを実装

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   end
   resources :anti_habit_records, only: %i[ create destroy ]
   resources :users, only: %i[ show ]
+  get "tags", to: "tags#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
