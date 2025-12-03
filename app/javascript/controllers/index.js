be8dccabec096a3f08a3ab408c48d5ada3a09c25
yuekami <yuekami7@gmail.com>
2025-12-03 23:04:23 +0900

    feat: stimulus-autocompleteインストール

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import { Autocomplete } from "stimulus-autocomplete"
+application.register("autocomplete", Autocomplete)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
