abf455ba0c41ce1ca8a7ad842e97de1f6d6e87a7
yuekami <yuekami7@gmail.com>
2025-12-15 11:04:29 +0900

    feat: rails_charts, echartsをインストール

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// echartsをグローバルに公開
+import * as echarts from 'echarts';
+window.echarts = echarts;
