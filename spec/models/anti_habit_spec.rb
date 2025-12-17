71085b2c49619d8ec89c426c4fb8a56be9f9a065
yuekami <yuekami7@gmail.com>
2025-12-17 23:20:04 +0900

    test: AntiHabitのモデルスペック(スコープ周りのテストを追加)

--- spec/models/anti_habit_spec.rb
+++ spec/models/anti_habit_spec.rb
@@ -52,4 +52,66 @@ RSpec.describe AntiHabit, type: :model do
       end
     end
   end
+
+  describe 'scopes' do
+    describe '.tagged_with' do
+      let(:user) { create(:user) }
+      let(:tag1) { Tag.create!(name: 'タグ1') }
+      let(:tag2) { Tag.create!(name: 'タグ2') }
+      let!(:anti_habit1) { create(:anti_habit, user: user, tags: [tag1]) }
+      let!(:anti_habit2) { create(:anti_habit, user: user, tags: [tag2]) }
+      let!(:anti_habit3) { create(:anti_habit, user: user, tags: [tag1, tag2]) }
+
+      it '指定したタグを持つAntiHabitを取得する' do
+        result = AntiHabit.tagged_with('タグ1')
+        expect(result).to include(anti_habit1, anti_habit3)
+        expect(result).not_to include(anti_habit2)
+      end
+
+      it '存在しないタグでは空を返す' do
+        result = AntiHabit.tagged_with('存在しないタグ')
+        expect(result).to be_empty
+      end
+    end
+
+    describe '.publicly_visible' do
+      let(:user) { create(:user) }
+      let!(:public_anti_habit1) { create(:anti_habit, user: user, is_public: true) }
+      let!(:public_anti_habit2) { create(:anti_habit, user: user, is_public: true) }
+      let!(:private_anti_habit) { create(:anti_habit, user: user, is_public: false) }
+
+      it '公開されているAntiHabitのみを取得する' do
+        result = AntiHabit.publicly_visible
+        expect(result).to include(public_anti_habit1, public_anti_habit2)
+        expect(result).not_to include(private_anti_habit)
+      end
+    end
+  end
+
+  describe '#today_record' do
+    let(:user) { create(:user) }
+    let(:anti_habit) { create(:anti_habit, user: user) }
+
+    context '今日の記録がある場合' do
+      let!(:today_record) { create(:anti_habit_record, anti_habit: anti_habit, recorded_on: Time.zone.today) }
+
+      it '今日の記録を返す' do
+        expect(anti_habit.today_record).to eq(today_record)
+      end
+    end
+
+    context '今日の記録がない場合' do
+      let!(:yesterday_record) { create(:anti_habit_record, anti_habit: anti_habit, recorded_on: Time.zone.yesterday) }
+
+      it 'nilを返す' do
+        expect(anti_habit.today_record).to be_nil
+      end
+    end
+
+    context '記録が全くない場合' do
+      it 'nilを返す' do
+        expect(anti_habit.today_record).to be_nil
+      end
+    end
+  end
 end
