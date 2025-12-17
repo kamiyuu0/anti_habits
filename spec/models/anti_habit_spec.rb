b32daf72e767f5cfed81d1800dea78340c34870b
yuekami <yuekami7@gmail.com>
2025-12-17 23:34:29 +0900

    test: AntiHabitのモデルスペック(簡易なビジネスロジックのテストを追加)

--- spec/models/anti_habit_spec.rb
+++ spec/models/anti_habit_spec.rb
@@ -58,9 +58,9 @@ RSpec.describe AntiHabit, type: :model do
       let(:user) { create(:user) }
       let(:tag1) { Tag.create!(name: 'タグ1') }
       let(:tag2) { Tag.create!(name: 'タグ2') }
-      let!(:anti_habit1) { create(:anti_habit, user: user, tags: [tag1]) }
-      let!(:anti_habit2) { create(:anti_habit, user: user, tags: [tag2]) }
-      let!(:anti_habit3) { create(:anti_habit, user: user, tags: [tag1, tag2]) }
+      let!(:anti_habit1) { create(:anti_habit, user: user, tags: [ tag1 ]) }
+      let!(:anti_habit2) { create(:anti_habit, user: user, tags: [ tag2 ]) }
+      let!(:anti_habit3) { create(:anti_habit, user: user, tags: [ tag1, tag2 ]) }
 
       it '指定したタグを持つAntiHabitを取得する' do
         result = AntiHabit.tagged_with('タグ1')
@@ -111,6 +111,130 @@ RSpec.describe AntiHabit, type: :model do
     context '記録が全くない場合' do
       it 'nilを返す' do
         expect(anti_habit.today_record).to be_nil
+      end
+    end
+  end
+
+  describe '#goal_reached?' do
+    let(:user) { create(:user) }
+    let(:anti_habit) { create(:anti_habit, user: user, goal_days: 5) }
+
+    context 'goal_daysがnilの場合' do
+      it 'falseを返す' do
+        anti_habit.goal_days = nil
+        expect(anti_habit.goal_reached?).to be false
+      end
+    end
+
+    context '連続達成日数がgoal_daysより少ない場合' do
+      it 'falseを返す' do
+        travel_to Time.zone.today do
+          create(:anti_habit_record, anti_habit: anti_habit, recorded_on: Time.zone.today)
+          create(:anti_habit_record, anti_habit: anti_habit, recorded_on: Time.zone.yesterday)
+          # 連続2日、goal_daysは5日
+          expect(anti_habit.goal_reached?).to be false
+        end
+      end
+    end
+
+    context '連続達成日数がgoal_daysと等しい場合' do
+      it 'trueを返す' do
+        travel_to Time.zone.today do
+          5.times do |i|
+            create(:anti_habit_record, anti_habit: anti_habit, recorded_on: i.days.ago)
+          end
+          # 連続5日、goal_daysは5日
+          expect(anti_habit.goal_reached?).to be true
+        end
+      end
+    end
+
+    context '連続達成日数がgoal_daysより多い場合' do
+      it 'trueを返す' do
+        travel_to Time.zone.today do
+          7.times do |i|
+            create(:anti_habit_record, anti_habit: anti_habit, recorded_on: i.days.ago)
+          end
+          # 連続7日、goal_daysは5日
+          expect(anti_habit.goal_reached?).to be true
+        end
+      end
+    end
+  end
+
+  describe '#calendar_data' do
+    let(:user) { create(:user) }
+    let(:anti_habit) { create(:anti_habit, user: user) }
+
+    context 'デフォルト（90日間）の場合' do
+      it '90日分のデータを生成する' do
+        travel_to Time.zone.today do
+          result = anti_habit.calendar_data
+          expect(result[:data].size).to eq(90)
+        end
+      end
+
+      it '正しい日付範囲のデータを含む' do
+        travel_to Time.zone.today do
+          result = anti_habit.calendar_data
+          dates = result[:data].map { |d| d[0] }
+          expect(dates.first).to eq((Time.zone.today - 89.days).to_s)
+          expect(dates.last).to eq(Time.zone.today.to_s)
+        end
+      end
+    end
+
+    context 'カスタム日数（30日間）の場合' do
+      it '30日分のデータを生成する' do
+        travel_to Time.zone.today do
+          result = anti_habit.calendar_data(days: 30)
+          expect(result[:data].size).to eq(30)
+        end
+      end
+    end
+
+    context '記録がある場合' do
+      it '記録がある日は1、ない日は0を返す' do
+        travel_to Time.zone.today do
+          create(:anti_habit_record, anti_habit: anti_habit, recorded_on: Time.zone.today)
+          create(:anti_habit_record, anti_habit: anti_habit, recorded_on: 2.days.ago)
+
+          result = anti_habit.calendar_data(days: 5)
+          data_hash = result[:data].to_h
+
+          expect(data_hash[Time.zone.today.to_s]).to eq(1)
+          expect(data_hash[2.days.ago.to_date.to_s]).to eq(1)
+          expect(data_hash[1.day.ago.to_date.to_s]).to eq(0)
+        end
+      end
+    end
+  end
+
+  describe '#tag_names_as_string' do
+    let(:user) { create(:user) }
+    let(:anti_habit) { create(:anti_habit, user: user) }
+
+    context 'タグがない場合' do
+      it '空文字列を返す' do
+        expect(anti_habit.tag_names_as_string).to eq('')
+      end
+    end
+
+    context 'タグが1つの場合' do
+      it 'そのタグ名を返す' do
+        tag = Tag.create!(name: 'テストタグ')
+        anti_habit.tags << tag
+        expect(anti_habit.tag_names_as_string).to eq('テストタグ')
+      end
+    end
+
+    context 'タグが複数の場合' do
+      it 'カンマ区切りの文字列を返す' do
+        tag1 = Tag.create!(name: 'タグ1')
+        tag2 = Tag.create!(name: 'タグ2')
+        tag3 = Tag.create!(name: 'タグ3')
+        anti_habit.tags << [ tag1, tag2, tag3 ]
+        expect(anti_habit.tag_names_as_string).to eq('タグ1, タグ2, タグ3')
       end
     end
   end
