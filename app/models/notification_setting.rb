69a49dcc4b6436473165bc89e59d5e67c41ed1cd
yuekami <yuekami7@gmail.com>
2026-02-16 23:22:34 +0900

    feat: NotificationSettingにvalidationを追加

--- app/models/notification_setting.rb
+++ app/models/notification_setting.rb
@@ -1,3 +1,7 @@
 class NotificationSetting < ApplicationRecord
   belongs_to :anti_habit
+  validates :notification_time, presence: true
+  validates :notification_enabled, inclusion: { in: [ true, false ] }
+  validates :notify_on_reaction, inclusion: { in: [ true, false ] }
+  validates :notify_on_comment, inclusion: { in: [ true, false ] }
 end
