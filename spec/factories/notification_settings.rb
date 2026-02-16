7ed851bfd593878eeb0c8bfcf0e8666fe3c3af2f
yuekami <yuekami7@gmail.com>
2026-02-16 23:23:02 +0900

    test: NotificationSettingのvalidationテストを追加

--- spec/factories/notification_settings.rb
+++ spec/factories/notification_settings.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     notification_time { "2025-10-28 22:21:01" }
     notify_on_reaction { false }
     notify_on_comment { false }
+    notification_enabled { false }
     association :anti_habit
   end
 end
--- spec/models/notification_setting_spec.rb
+++ spec/models/notification_setting_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe NotificationSetting, type: :model do
   describe 'associations' do
     it { should belong_to(:anti_habit) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:notification_time) }
+    it { should allow_value(true).for(:notification_enabled) }
+    it { should allow_value(false).for(:notification_enabled) }
+    it { should_not allow_value(nil).for(:notification_enabled) }
+    it { should allow_value(true).for(:notify_on_reaction) }
+    it { should allow_value(false).for(:notify_on_reaction) }
+    it { should_not allow_value(nil).for(:notify_on_reaction) }
+    it { should allow_value(true).for(:notify_on_comment) }
+    it { should allow_value(false).for(:notify_on_comment) }
+    it { should_not allow_value(nil).for(:notify_on_comment) }
+  end
 end
